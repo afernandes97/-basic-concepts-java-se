7ac84acdda6751a84ab06771c98eda42e9f30b3e
afernandes97 <andreluissilvafernandes@gmail.com>
2022-09-08 13:49:31 -0300

    update fundamentals java se

---
+++ src/fundamentals/PrimitiveVsObject.java
@@ -0,0 +1,12 @@
+package fundamentals;
+
+public class PrimitiveVsObject {
+    public static void main(String[] args) {
+        String s = "test text";
+        s = s.toUpperCase();
+
+        //with an object we can access the properties using a "." , except this primitive variables: byte, short, int, long, double, float, boolean, char.
+
+        //Wrappers: is object version of primitive types, to more explications access the class: Wrappers.
+    }
+}

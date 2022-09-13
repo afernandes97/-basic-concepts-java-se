4dccbd8331ff0b4a19e3d5e76420c719a2aaeba9
afernandes97 <andreluissilvafernandes@gmail.com>
2022-09-13 09:52:17 -0300

    adding arithmetics and arithmetics challenger

---
+++ src/fundamentals/Arithmetics.java
@@ -0,0 +1,29 @@
+package fundamentals;
+
+public class Arithmetics {
+    public static void main(String[] args) {
+        //Arithmetics with console log
+        System.out.println(2 * 5);
+        //inference
+        var x = 34.33;
+        var y = 2.2;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a / (double)b);//casting
+        System.out.println(a / (float)b);//casting
+
+        //rest of division, using %
+        System.out.println( 10 % 4);
+    }
+}
---
+++ src/fundamentals/ArithmeticsChallenge.java
@@ -0,0 +1,25 @@
+package fundamentals;
+
+public class ArithmeticsChallenge {
+    public static void main(String[] args) {
+        
+        double aPow = Math.pow(6 * (3+2),2);
+        double aMultiply = 3*2;
+
+
+        double b = (1-5)*(2-7);
+        double bNumber = 2;
+
+        double aMath = aPow / aMultiply;
+
+        double bMath = Math.pow(b / bNumber, 2);
+
+        double superiorResult = Math.pow(aMath - bMath,3);
+
+        double c = Math.pow(10,3);
+
+        double finalResult = superiorResult / c;
+        System.out.println("The result of operation is: " + finalResult);
+
+    }
+}

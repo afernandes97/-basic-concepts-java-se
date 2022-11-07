26d5e797102d942fdedb21e232e7bb64ac3ea875
afernandes97 <andreluissilvafernandes@gmail.com>
2022-11-07 10:12:55 -0300

    adding savingsaccount and upcasting e downcasting superclass & subclass

--- src/inheritance/application/Program.java
+++ src/inheritance/application/Program.java
@@ -1,7 +1,37 @@
 package inheritance.application;
 
+import inheritance.entities.Account;
+import inheritance.entities.BusinessAccount;
+import inheritance.entities.SavingsAccount;
+
 public class Program {
     public static void main(String[] args){
+        Account acc = new Account(1001,"André",0.0);
+        BusinessAccount bcc = new BusinessAccount(1002,"Maria",0.0,1000.00);
+
+        //UPCASTING BusinessAccount && SavingsAccount => Account (subclass to superclass)
+        Account acc1 = bcc;
+        Account acc2 = new BusinessAccount(1003,"bob",0.0,1000.00);
+        Account acc3 = new SavingsAccount(1004,"Job", 0.0, 0.02);
+
+
+        //DOWNCASTING SuperClass to SubClass
+        BusinessAccount acc4 = (BusinessAccount)acc2; //implicit convert
+        acc4.loan(150.00);
+
+        //verify instance type using instaceof
+        if(acc3 instanceof BusinessAccount){
+            BusinessAccount acc5 = (BusinessAccount) acc3;
+            acc5.loan(200.00);
+            System.out.println("LOAN");
+        }
+
+        //verify instance type using instaceof
+        if(acc3 instanceof SavingsAccount){
+            SavingsAccount acc5 = (SavingsAccount) acc3;
+            acc5.updateBalance();
+            System.out.println("Update");
+        }
 
     }
 }

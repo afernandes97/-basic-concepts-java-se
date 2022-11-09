f7da0d5e5f44f89937924420f5156c69957d4edf
afernandes97 <andreluissilvafernandes@gmail.com>
2022-11-09 16:42:32 -0300

    update program to read user data

--- src/tryCatchExamples/TryCatchTest/application/Program.java
+++ src/tryCatchExamples/TryCatchTest/application/Program.java
@@ -1,5 +1,25 @@
 package tryCatchExamples.TryCatchTest.application;
 
-public class Program {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
+public class Program {
+    //sdf.parse might throw an exception, to resolve this usage throws ParseException to alert the compiler that the main method might throw an exception
+    public static void main(String[] args) throws ParseException {
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Room number: ");
+        int roomNumber = sc.nextInt();
+
+        System.out.println("Check-in date (dd/MM/yyyy): ");
+        Date checkIn = sdf.parse(sc.next());
+
+        System.out.println("Check-out date (dd/MM/yyyy): ");
+        Date checkOut = sdf.parse(sc.next());
+
+
+    }
 }

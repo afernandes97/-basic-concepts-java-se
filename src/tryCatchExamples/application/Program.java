45dac89b97a4bf9de4f700d6c864c44778b5bd50
afernandes97 <andreluissilvafernandes@gmail.com>
2022-11-09 10:56:52 -0300

    creating package for study try catch exception.

---
+++ src/tryCatchExamples/application/Program.java
@@ -0,0 +1,26 @@
+package tryCatchExamples.application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Integer lengthVector = 0;
+
+        //When executing Java code, different errors can occur, one of the ways to treat them is using try catch, to treat possibles errors
+        try {
+            String[] vector = sc.nextLine().split(" ");
+            lengthVector = vector.length;
+            int position = sc.nextInt();
+            System.out.println(vector[position]);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Invalid Position,\n" +
+                    "please enter the number between 0 and " + (lengthVector - 1));
+        }catch (InputMismatchException e){
+            System.out.println("Please entry only numbers");
+        }
+        System.out.println("End of program");
+        sc.close();
+    }
+}

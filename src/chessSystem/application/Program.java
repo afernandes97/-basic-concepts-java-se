ac3975700a1accb9d53adb5152f336ad89c8c71d
afernandes97 <andreluissilvafernandes@gmail.com>
2022-11-11 12:44:58 -0300

    challange: create a chess board game. Adding first class: Position

---
+++ src/chessSystem/application/Program.java
@@ -0,0 +1,10 @@
+package chessSystem.application;
+
+import chessSystem.boardgame.Position;
+
+public class Program {
+    public static void main(String[] args){
+        Position pos = new Position(3,5);
+        System.out.println(pos);
+    }
+}

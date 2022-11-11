35e14eaba00b7cfd99ce10ee4cc624fb50c7d6e3
afernandes97 <andreluissilvafernandes@gmail.com>
2022-11-11 12:59:50 -0300

    Adding class and implementing: Board and piece

--- src/chessSystem/application/Program.java
+++ src/chessSystem/application/Program.java
@@ -1,10 +1,15 @@
 package chessSystem.application;
 
+import chessSystem.boardgame.Board;
 import chessSystem.boardgame.Position;
 
 public class Program {
     public static void main(String[] args){
+        //test: instantiate position
         Position pos = new Position(3,5);
         System.out.println(pos);
+
+        //test: instantiate chess board
+        Board board = new Board(8,8);
     }
 }
---
+++ src/chessSystem/boardgame/Piece.java
@@ -0,0 +1,16 @@
+package chessSystem.boardgame;
+
+public class Piece {
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
+}
